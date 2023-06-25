65ba90a75df1e0ef7750d30b98be9cd6eb7ace32
MochitoSpooky <119640815+MochitoSpooky@users.noreply.github.com>
2023-06-25 16:05:51 -0400

    Update models.py

--- estudio/principal/models.py
+++ estudio/principal/models.py
@@ -7,3 +7,12 @@ class Usuario(models.Model):
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
+
+class Galeria(models.Model):
+    imagen = models.ImageField(upload_to='principal/img')
+    nombre = models.CharField(max_length=100)
+    autor = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
