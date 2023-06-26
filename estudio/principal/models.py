f99e5162c1ce919cfd58afbc9632290c721b5c41
MochitoSpooky <119640815+MochitoSpooky@users.noreply.github.com>
2023-06-26 10:30:02 -0400

    Update models.py

--- estudio/principal/models.py
+++ estudio/principal/models.py
@@ -9,17 +9,10 @@ class Usuario(models.Model):
         return f"{self.nombre} {self.apellido}"
 
 class Galeria(models.Model):
-    imagen = models.ImageField(upload_to='principal/img')
     nombre = models.CharField(max_length=100)
     autor = models.CharField(max_length=100)
+    imagen = models.ImageField(upload_to='principal/img')
     valor = models.DecimalField(max_digits=20, decimal_places=2)
 
     def __str__(self):
-        return self.get_nombre_completo()
-
-
-    def get_nombre_completo(self):
         return self.nombre
-
-    def get_autor_personalizado(self):
-        return self.autor
